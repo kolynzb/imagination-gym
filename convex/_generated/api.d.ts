188753489a51969dcc672d0f3d8771bfa206c3bb
kolynzb <collinsbenda360@gmail.com>
2026-09-22 18:06:02 +0300

    chore(convex): update generated API types

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -9,6 +9,7 @@
  */
 
 import type * as crew from "../crew.js";
+import type * as http from "../http.js";
 
 import type {
   ApiFromModules,
@@ -18,6 +19,7 @@ import type {
 
 declare const fullApi: ApiFromModules<{
   crew: typeof crew;
+  http: typeof http;
 }>;
 
 /**
